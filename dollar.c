f354751416d7d5099d03aa72f8a54c14059ec224
Yusuf <yusuftaha9@gmail.com>
2023-01-19 13:56:29 +0300

    Fix dollar. Its muk now

--- dollar.c
+++ dollar.c
@@ -9,33 +9,35 @@ char	*status_case(char *s, int *i)
 	return (str);
 }
 
+char	*handle_env_substr(const char *env_var, char *command)
+{
+	char	*new;
+
+	new = ft_strdup(ft_substr(env_var, ft_strlen(command), ft_strlen(env_var)));
+	free(command);
+	return (new);
+}
+
+// MUK
 char	*dollar(char *s, int *i)
 {
 	int		j;
-	int		check;
 	char	*command;
 
 	j = 0;
-	check = 0;
-	command = (char *)malloc(ft_strlen(s) * sizeof(char));
+	command = (char *)malloc((ft_strlen(s) + 10) * sizeof(char));
 	(*i)++;
 	if (s[*i] == '?')
-		return (status_case(s, i));
-	while (s[*i] != '\0' && s[*i] != ' ' && s[*i] != '\t' && s[*i] != '>' \
-		&& s[*i] != '<' && s[*i] != '"' && s[*i] != '$')
+		return (free(command), status_case(s, i));
+	if (!ft_isalnum(s[*i]) && s[*i] != '_')
+		return (free(command), ft_strdup("$"));
+	while (ft_isalnum(s[*i]) || s[*i] == '_')
 		command[j++] = s[(*i)++];
 	command[j] = '=';
 	command[j + 1] = '\0';
 	j = -1;
 	while (++j < ft_str2len(g_x->export))
-	{
-		if (ft_strnstr(g_x->export[j], command, ft_strlen(command)))
-		{
-			command = ft_substr(g_x->export[j], ft_strlen(command), ft_strlen(g_x->export[j]));
-			check = 1;
-		}
-	}
-	if (check == 0)
-		return ("");
-	return (command);
+		if (ft_strnstr(g_x->export[j], command, ft_strlen(command)) != 0)
+			return (handle_env_substr(g_x->export[j], command));
+	return (ft_strdup(""));
 }
