85f7eb4338eb2692fa9cc2652321b00a12e5664f
Yusuf <yusuftaha9@gmail.com>
2023-01-19 18:28:51 +0300

    export fix

--- builtin/export_utils_two.c
+++ builtin/export_utils_two.c
@@ -4,7 +4,7 @@
 // There is no env variable with name ahmet
 void	export_add_var_equal(t_export_vars *exp, char *var)
 {
-	g_x->export = ft_rrealloc(g_x->export, ft_str2len(g_x->export) + 1);
+	g_x->export = ft_rrealloc(g_x->export, ft_str2len(g_x->export) + 2);
 	g_x->export[exp->i] = ft_strdup(var);
 	g_x->export[exp->i + 1] = 0;
 	g_x->export = export_sorter(g_x->export);
