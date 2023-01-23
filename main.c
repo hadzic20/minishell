55b9781ad29961894c07f2ed8389e9e0cb660297
Yusuf <yusuftaha9@gmail.com>
2023-01-23 15:50:57 +0300

    free falan

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: amillahadzic <amillahadzic@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 19:57:16 by amillahadzi       #+#    #+#             */
-/*   Updated: 2023/01/22 20:28:32 by amillahadzi      ###   ########.fr       */
+/*   Updated: 2023/01/23 15:49:31 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ void	free_them_all_mf(void)
 	i = 0;
 	while (i < g_x->cmd_count)
 	{
-		free(g_x->cmds[i].raw_command);
-		ft_free(g_x->cmds[i].handled_cmd);
+		if (g_x->cmds[i].raw_command != NULL)
+			free(g_x->cmds[i].raw_command);
+		if (g_x->cmds[i].handled_cmd != NULL)
+			ft_free(g_x->cmds[i].handled_cmd);
 		i++;
 	}
 	free(g_x->cmds);
@@ -67,9 +69,9 @@ int	main(int argc, char **argv, char **envp)
 	g_x = malloc(sizeof(t_x));
 	g_x->export = storage_and_copy(envp);
 	g_x->export = export_sorter(g_x->export);
-	g_x->error_code = 42;
-	// signal(SIGINT, handle_signal);
-	// signal(SIGQUIT, SIG_IGN);
+	g_x->error_code = 0;
+	signal(SIGINT, handle_signal);
+	signal(SIGQUIT, SIG_IGN);
 	// testextract("> $? $? ahmet  1   2");
 	// testextract("1   2\"\"\"\"> ahmet<asd >sad <bas>>asd <<adsf >>>omg42 3 4");
 	// testextract("  >ahmet");
@@ -80,6 +82,13 @@ int	main(int argc, char **argv, char **envp)
 	// testextract("  >ahmet $USER\"whatt?? $USER\"");
 	// testextract("  >ahmet <heyo $USER\"whatt?? $USER\" > dostum");
 	// testextract("\"$USER%omg\"");
+	// testextract("asd>");
+	// testextract("<");
+	// testextract("< asd");
+	// testextract("  <");
+	// testextract("<<<<<<");
+	// testextract("<><><>");
+	// return (0);
 	// testextract("bwu$$h$USER%omg$$U$SE'$R$%as$d$$U$SER$'$a$'$ > 'dostum <najs || '");
 	// testextract("bwu>$$h$USER%o mg$$U$SE ' $R$%as$d$$U$SER$'$a$'$ > 'dostum <najs || '");
 	// testextract("bwu$$?h$USER% omg$$U $ SE'$R$%as$d$$U_)(*&^%$#@!$SER$'$a$'$ > 'dostum <najs || '");
